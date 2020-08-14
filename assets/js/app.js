ace9bd4bd1f6ed88d64de3df0ca9623d47dd04b9
Aleksandr18 <aleksandrperetertov1@gmail.com>
2020-08-14 17:10:11 +0300

    version 5

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+     $(".owl-curse").owlCarousel();
+});
+
+$('.owl-curse').owlCarousel({
+     loop:false,
+     margin:24,
+     nav:true,
+     responsive: {
+          300: {
+               items: 3
+          }
+     }
+})
+
