11c1a109913f425a0a8a145bac92f678765ea13e
Aleksandr <35434160+Aleksandr18@users.noreply.github.com>
2020-08-27 16:05:28 +0300

    talhuyeroj

--- project.local/assets/js/app.js
+++ project.local/assets/js/app.js
@@ -45,3 +45,7 @@ $(function() {
     });
 });
 
+
+document.querySelector('.burger').addEventListener('click', function(){
+    document.querySelector('.burger span').classList.toggle('active');
+})
